1e46a03ef77aaec1f8a5210874569c7989161b43
HenriqueAdamiKruger <88462883+HenriqueAdamiKruger@users.noreply.github.com>
2022-02-17 21:15:45 -0300

    Delete index.php

--- TemplateHTML/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "lib2/li.class.php";
-include "lib2/ul.class.php";
-include "lib2/html.class.php";
-
-$html = new html("pt-br","UTF-8","Teste - Henrique Adami Krüger - BSN","");
-
-echo $html;
