b7a735d896e2533017cdf7e2e84c326643ff473c
HenriqueAdamiKruger <88462883+HenriqueAdamiKruger@users.noreply.github.com>
2022-02-17 21:18:39 -0300

    Add files via upload

---
+++ TeamplateHTML/index.php
@@ -0,0 +1,8 @@
+<?php
+include "lib2/li.class.php";
+include "lib2/ul.class.php";
+include "lib2/html.class.php";
+
+$html = new html("pt-br","UTF-8","Teste - Henrique Adami Krüger - BSN","");
+
+echo $html;
